37ea7e82b2c1f41d8ab041f2e502f9f33d2c94fb
minseo0228 <0228minseokim@gmail.com>
2023-01-31 11:03:51 +0900

    20230130

--- Silver/1105.py
+++ Silver/1105.py
@@ -1,11 +1,4 @@
 a,b = input().split()
-# def search(x):
-#     count = 0
-#     for i in x:
-#         if i == '8':
-#             count += 1
-#     return count
-# num = 9999
 if len(a) != len(b):
     print(0)
 else:
@@ -14,26 +7,8 @@ else:
         if a[i] == b[i]:
             if a[i] == '8':
                 c += 1
-    if a[0] != b[0]:
-        c -= 1
+        else:
+            break
     print(c)
 
-# if len(a) <= 1:
-#     print(0)
-# elif len(a) != len(b):
-#     for i in range(int(a),int(b)+1):
-#         num = min(search(str(i)),num)
-#     print(num)
-# else:
-#     c = 0
-#     for i in range(len(a)):
-#         if a[i] == b[i] and a[i] == '8':
-#             c += 1
-#         elif a[i] == b[i]:
-#             continue
-#         else:
-#             for j in range():
-#                 num = min(search(str(j)),num)
-#     print(c+num)
-     
    
--- Silver/1932.py
+++ Silver/1932.py
@@ -4,27 +4,18 @@ dp = [[0]*(i) for i in range(1,n+1)]
 for i in range(n):
     graph.append(list(map(int,input().split())))
 dp[0][0]= graph[0][0]
-for i in range(1,n):
-    dp[i][0] = dp[i-1][0] + graph[i][0]
-    dp[i][-1] = dp[i-1][-1] + graph[i][-1]
-    for j in range(1,i-1):
-        dp[i][j] = max(dp[i-1][j-1] + graph[i][j], dp[i-1][j] + graph[i][j])
-print(max(dp[n-1]))
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0]*(x+1) for x in range(n)]
-for i in range(n-1):
-    for j in range(len(arr[i])):
-        if i ==0 :
-            dp[i+1][j] = arr[i][j] + arr[i+1][j]
-            dp[i+1][j+1] = arr[i][j] + arr[i+1][j+1]
+# for i in range(1,n):
+#     dp[i][0] = dp[i-1][0] + graph[i][0]
+#     dp[i][-1] = dp[i-1][-1] + graph[i][-1]
+#     for j in range(1,i-1):
+#         dp[i][j] = max(dp[i-1][j-1] + graph[i][j], dp[i-1][j] + graph[i][j])
+# print(max(dp[n-1]))
+for j in range(1,n):
+    for i in range(len(graph[j])):
+        if i == 0:
+            dp[j][0] = dp[j-1][0] + graph[j][0]
+        elif i == len(graph[j])-1:
+            dp[j][-1] = dp[j-1][-1] + graph[j][-1]
         else:
-            if dp[i+1][j] < dp[i][j] + arr[i+1][j]:
-                dp[i+1][j] = dp[i][j] + arr[i+1][j]
-            dp[i+1][j+1] = dp[i][j] + arr[i+1][j+1]
-
-if n == 1: # 1일때는 for문을 타지 않기 때문에 따로 분기처리
-    print(arr[0][0])
-else:
-    result = max(dp[n-1])
-    print(result)
+            dp[j][i] = max(dp[j-1][i-1] + graph[j][i], dp[j-1][i] + graph[j][i])
+print(max(dp[-1]))
