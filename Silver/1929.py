60c1c89edcdc25087dfd99ec82c660e6cb466ed8
minseo0228 <0228minseokim@gmail.com>
2023-02-25 09:14:34 +0900

    20230225

---
+++ Silver/1929.py
@@ -0,0 +1,14 @@
+M,N = map(int,input().split())
+prime = [0]*(N+1)
+prime[0] = 1
+prime[1] = 1
+for i in range(2,N+1):
+    for j in range(2,N+1):
+        if i*j > N:
+            break
+        prime[i*j] = 1
+
+print(prime)
+for i in range(M,N+1):
+    if prime[i] == 0:
+        print(i)
