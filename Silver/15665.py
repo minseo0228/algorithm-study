71a89d50824a9e0d2b3d4077df8e571d12ff7bea
minseo0228 <0228minseokim@gmail.com>
2023-03-14 23:33:00 +0900

    20230314

---
+++ Silver/15665.py
@@ -0,0 +1,27 @@
+from itertools import product
+N,M = map(int,input().split())
+num = list(input().split())
+num.sort()
+numlist = list(product(num,repeat=M))
+numlist = sorted(list(set(numlist)))
+for i in numlist:
+    print(''.join(i))
+////
+from itertools import product
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+a.sort()
+cmb = list(product(a, repeat=m))
+ans = []
+
+for c in cmb:
+    ans.append(c)
+
+ans = sorted(list(set(ans))) 
+
+for c in ans:
+    for i in c:
+        print(i, end=' ')
+    print()
