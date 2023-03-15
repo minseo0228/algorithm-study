cdd8bdc7f226d7d363b2ac0ecaf4db4fafdc5087
minseo0228 <0228minseokim@gmail.com>
2023-03-15 20:36:57 +0900

    20230315

---
+++ Silver/1448.py
@@ -0,0 +1,18 @@
+N = int(input())
+num = []
+for _ in range(N):
+    num.append(int(input()))
+num.sort(reverse=True)
+
+flag = 1
+for i in range(N-2):
+    if num[i] < num[i+1] + num[i+2]:
+        print(num[i] + num[i+1] + num[i+2])
+        flag = 0
+        break
+
+if flag == 1:
+    print(-1)
+
+
+
