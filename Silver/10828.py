19947e56d47fe0beee94b6bfa6d8b5b72d241630
unknown <0228minseokim@gmail.com>
2023-07-24 15:02:16 +0900

    20230724

---
+++ Silver/10828.py
@@ -0,0 +1,23 @@
+n = int(input())
+stack = []
+for i in range(n):
+    a = list(input().split())
+    if a[0] == 'push':
+        stack.append(a[1])
+    if a[0] == 'pop':
+        if len(stack)== 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    if a[0] == 'size':
+        print(len(stack))
+    if a[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    if a[0] == 'top':
+        if len(stack)== 0:
+            print(-1)
+        else:
+            print(stack[-1])
