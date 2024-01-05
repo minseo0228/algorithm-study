0be8456a87fd17a847ba8d24db3aeb77d61c0175
unknown <0228minseokim@gmail.com>
2024-01-05 10:26:40 +0900

    20240105

---
+++ Gold/11729.py
@@ -0,0 +1,12 @@
+n = int(input())
+list1 = []
+list2 = []
+list3 = []
+## 하노이탑 원리에 의해서 이동수 = 2를 n번 만큼 곱한값 -1
+k = 2^n -1
+for i in range(n):
+        list1.append(n-i)
+def hanoi(n):
+        
+    
+    
---
+++ Silver/14501.py
@@ -0,0 +1,47 @@
+# n = int(input())
+# timelist = []
+# for i in range(n):
+#     a,b = map(int, input().split())
+#     timelist.append([a,b])
+# # print(timelist)
+
+# max_money = 0
+# visit =[0]*n
+# for i in range(n-1):
+#     if visit[i] == 0:
+#         start = i + 1
+#         money = timelist[i][1]
+#         time = timelist[i][0]
+#         while True:
+#             if start + time >= n:
+#                 if start + time == n and  timelist[n-1][0] == 1:
+#                     money += timelist[n-1][1]
+#                 break
+#             else:
+#                 start += time
+#                 money += timelist[start-1][1]
+#                 a = timelist[start-1][0]
+#     # print(money)
+#     if money >= max_money:
+#         max_money = money
+
+# print(max_money)
+
+    
+
+
+
+
+n = int(input())
+list1 = []
+for _ in range(n):
+    list1.append(list(map(int,input().split())))
+
+dp = [0]*(n+1)
+
+for i in range(n):
+    for j in range(i + list1[i][0],n+1):
+        if dp[j] < dp[i] + list1[i][1]:
+            dp[j] = dp[i] + list1[i][1]
+print(dp)
+print(max(dp))
