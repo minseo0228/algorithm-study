8495bdb424342cc40a76245dcf9fb495a61afb82
unknown <0228minseokim@gmail.com>
2024-01-29 15:45:44 +0900

    20240129

--- Silver/11660.py
+++ Silver/11660.py
@@ -1,21 +1,22 @@
 import sys
-N, M = map(int, sys.stdin.readline().strip().split())
 
-graph = []
-for _ in range(N):
-    list1 = list(map(int,sys.stdin.readline().strip().split()))
-    graph.append(list1)
+n,m = map(int, sys.stdin.readline().split())
+numlist = []
+for i in range(n):
+    numlist.append(list(map(int, sys.stdin.readline().split())))
+l = len(numlist[0])
+# def calnum(x1,y1,x2,y2,numlist):
+#     s = 0
+#     for i in range(x1-1,x2):
+#         for j in range(y1-1,y2):
+#             s += numlist[j][i]
+#     return s
 
-def calsum(x,y,nx,ny):
-    sum = 0
-    for i in range(x,nx+1):
-        for j in range(y,ny+1):
-            sum += graph[i-1][j-1]
-    return(sum)
-
-for i in range(M):
-    x,y,nx,ny = map(int,input().split())
-    if x == nx and y == ny:
-        print(graph[x-1][y-1])
-    else:
-        print(calsum(x,y,nx,ny))
+ps = [[0]*(l+1) for _ in range(n+1)]
+for i in range(0,l+1):
+    for j in range(0,n+1):
+        ps[i][j] = ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1] + numlist[i-1][j-1]
+# print(ps)
+for _ in range(m):
+    x1,y1,x2,y2 = map(int, sys.stdin.readline().split())
+    print(ps[x2][y2] - ps[x2][y1-1] - ps[x1-1][y2] + ps[x1-1][y1-1]) 
---
+++ Silver/14888.py
@@ -0,0 +1,6 @@
+n = int(input())
+num = map(int,list(input().split()))
+l = map(int,list(input().split()))
+
+sn = 0
+bn = 0
