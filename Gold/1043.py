5334b84b51237a6593c85f0aab60ec08c3da1432
unknown <0228minseokim@gmail.com>
2024-02-01 10:35:29 +0900

    20240201

---
+++ Gold/1043.py
@@ -0,0 +1,21 @@
+n, m = map(int,input().split())
+tlist = set(input().split()[1:])
+plist = []
+a = 0
+
+for i in range(m):
+    flag = 0
+    plist.append(set(input().split()[1:]))
+
+for _ in range(m):
+    for p in plist:
+        if p & tlist:
+            tlist = tlist.union(p)
+print(plist)
+print(tlist)
+for p in plist:
+    if p & tlist:
+        continue
+    a += 1
+
+print(a)
